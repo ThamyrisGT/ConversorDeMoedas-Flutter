f24e36013891109422efca45a1880610910ff342
Thamyris <tgthurler@gmail.com>
2021-09-21 21:55:16 -0300

    Finalizado o conversor, limpando os campos e convertendo nas 3 moedas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -37,8 +38,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  late final double dolar;
-  late final double euro;
+  final _realController = TextEditingController();
+  final _dolarController = TextEditingController();
+  final _euroController = TextEditingController();
+
+  double dolar = 0.0;
+  double euro = 0.0;
+
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    _dolarController.text = (real / dolar).toStringAsFixed(2);
+    _euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    _realController.text = (dolar * this.dolar).toStringAsFixed(2);
+    _euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    _realController.text = (euro * this.euro).toStringAsFixed(2);
+    _dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    _realController.text = "";
+    _dolarController.text = "";
+    _euroController.text = "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,35 +129,14 @@ class _HomeState extends State<Home> {
                         color: Colors.amber,
                       ),
                       Divider(),
-                      TextField(
-                        style: TextStyle(color: Colors.amber, fontSize: 25),
-                        decoration: InputDecoration(
-                            labelText: "Reais",
-                            labelStyle:
-                                TextStyle(color: Colors.amber, fontSize: 25),
-                            border: OutlineInputBorder(),
-                            prefixText: "R\$"),
-                      ),
+                      buildTextField(
+                          "Reais", "R\$", _realController, _realChanged),
                       Divider(),
-                      TextField(
-                        style: TextStyle(color: Colors.amber, fontSize: 25),
-                        decoration: InputDecoration(
-                            labelText: "Dólares",
-                            labelStyle:
-                                TextStyle(color: Colors.amber, fontSize: 25),
-                            border: OutlineInputBorder(),
-                            prefixText: "US\$"),
-                      ),
+                      buildTextField(
+                          "Dólares", "US\$", _dolarController, _dolarChanged),
                       Divider(),
-                      TextField(
-                        style: TextStyle(color: Colors.amber, fontSize: 25),
-                        decoration: InputDecoration(
-                            labelText: "Euros",
-                            labelStyle:
-                                TextStyle(color: Colors.amber, fontSize: 25),
-                            border: OutlineInputBorder(),
-                            prefixText: "£"),
-                      ),
+                      buildTextField(
+                          "Euros", "£", _euroController, _euroChanged),
                       Divider(),
                     ],
                   ),
@@ -127,4 +147,20 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+}
+
+Widget buildTextField(String label, String prefix, TextEditingController tec,
+    Function(String) f) {
+  return TextField(
+    controller: tec,
+    style: TextStyle(color: Colors.amber, fontSize: 25),
+    onChanged: f,
+    //keyboardType: TextInputType.number,
+    keyboardType: TextInputType.numberWithOptions(decimal: true), // números decimais para IOS
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber, fontSize: 25),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+  );
 }
